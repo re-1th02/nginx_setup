325595ffe759f1bc0aaad89bbd4be8937309ca71
re-1th02 <95493501+re-1th02@users.noreply.github.com>
2023-07-03 15:20:53 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ read -e -p "Edit Port : " -i 5000 port
 sed s/5000/${port}/g template > nginx.conf
 
 sudo apt-get update
-pkgs="make python3.9-venv nginx"
+pkgs="make python3.10-venv nginx"
 sudo apt-get install $pkgs
 make nginx
-python3.9 -m venv $HOME/env
+python3.10 -m venv $HOME/env
 echo 'alias require="python3 -m pip install -r requirements.txt";' $'\n' >> ~/.bashrc
 source ~/.bashrc
 source $HOME/env/bin/activate
